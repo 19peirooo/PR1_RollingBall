f7846f87f6618ae84db29f361cb2a64e8d0af509
19peirooo <160737016+19peirooo@users.noreply.github.com>
2026-03-03 23:37:49 +0100

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    [SerializeField] private float jumpForce = 15;
+    [SerializeField] private float moveForce = 40;
+    
+    private Rigidbody rb;
+    
+    private float hInput;
+    private float vInput;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        hInput = Input.GetAxisRaw("Horizontal");
+        vInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space) &&
+            Physics.Raycast(transform.position, Vector3.down, transform.localScale.z))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(hInput, 0, vInput).normalized;
+        rb.AddForce(movement * moveForce, ForceMode.Force);
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PR1_RollingBall
+Practica 1 de Rolling Ball Para la Asignatura de Fisica e Inteligencia Artificial Para Videojuegos
